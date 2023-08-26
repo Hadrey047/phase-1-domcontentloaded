76dee73e5c343069f3d6ca86fd3a797353402628
Olayanju Oluwadamilare Samuel <hadreytechsolutions@gmail.com>
2023-08-26 16:12:14 +0100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    console.log("The DOM has load")
+  });
+  
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+ 
+
+ 
